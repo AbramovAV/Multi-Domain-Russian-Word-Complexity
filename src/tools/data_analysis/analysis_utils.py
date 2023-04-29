60e27e63241d8660abd7eaf66b0c95584779d5fc
Alexey Abramov <abitualex@gmail.com>
2023-04-29 09:16:39 +0300

    added utils for dataframe aggregation and label projecting

--- src/tools/data_analysis/analysis_utils.py
+++ src/tools/data_analysis/analysis_utils.py
@@ -22,6 +22,59 @@ def add_freq_for_sentence(dataframe:pd.DataFrame, initial_dataframe:pd.DataFrame
     dataframe = dataframe.drop(labels=["start_idx", "context"], axis="columns")
     return dataframe
 
+
+def project_labels_into_contunious(dataframe:pd.DataFrame) -> pd.DataFrame:
+    dataframe["OUTPUT:complexity"] = (dataframe["OUTPUT:complexity"] - 1) * 0.25
+    return dataframe
+
+
+def project_labels_into_discrete(dataframe:pd.DataFrame) -> pd.DataFrame:
+    dataframe["OUTPUT:complexity"] = ((dataframe["OUTPUT:complexity"] / 0.25) + 1).astype(int)
+    return dataframe
+
+
+def aggregate_by_task(dataframe:pd.DataFrame, freq_range=None, auxillary_mapping={}) -> pd.DataFrame:
+    if freq_range is None:
+        dataframe = dataframe.groupby("ASSIGNMENT:task_id", sort=False).aggregate(
+        {"OUTPUT:complexity": "mean", "lemma": "first"} | auxillary_mapping
+        )
+    else:
+        dataframe = dataframe.groupby("ASSIGNMENT:task_id", sort=False).aggregate(
+        {"OUTPUT:complexity": "mean", "frequency(ipm)": "mean", "lemma": "first"} | \
+            auxillary_mapping
+        )
+    return dataframe
+
+
+def aggregate_by_lemma(dataframe:pd.DataFrame, freq_range=None, auxiliary_mapping={}) -> pd.DataFrame:
+    if freq_range is None:
+        dataframe = dataframe.groupby("lemma", sort=False).aggregate(
+        {"OUTPUT:complexity": "mean", "lemma": "first"} | auxiliary_mapping
+        )
+    else:
+        dataframe = dataframe.groupby("lemma", sort=False).aggregate(
+        {"OUTPUT:complexity": "mean", "frequency(ipm)": "mean", "lemma": "first"} | \
+            auxiliary_mapping
+        )
+    return dataframe
+
+def filter_by_freq_range(dataframe:pd.DataFrame, freq_range=None):
+    if freq_range is not None:
+        ids = (freq_range[0] <= dataframe["frequency(ipm)"]) & \
+            (dataframe["frequency(ipm)"] <= freq_range[1])
+        return dataframe[ids]
+
+
+def perform_t_test():
+    pass
+
+
+def filter_by_fast_responses(dataframe: pd.DataFrame, response_limit=15) -> pd.DataFrame:
+    ids = (dataframe["ASSIGNMENT:submitted"].dt.total_seconds() - \
+        dataframe["ASSIGNMENT:started"].dt.total_seconds()) >= response_limit
+    return dataframe[ids]
+
+
 if __name__=='__main__':
     dataframe = pd.read_csv("data/annotated/annotated_data/medline/assignments_from_pool_38509153__11-04-2023.tsv", sep='\t')
     initial_df = pd.read_csv("data/annotated/prepared_data/medline/data.tsv", sep='\t')
