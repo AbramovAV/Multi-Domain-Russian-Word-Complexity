4c6bc5895826d67ac142fb3b3a8d63f2b2a0298f
Alexey Abramov <abitualex@gmail.com>
2023-07-17 15:57:24 +0300

    code refactoring

--- src/model_training/dataset_utils.py
+++ src/model_training/dataset_utils.py
@@ -1,30 +1,53 @@
+"""
+Module with various utils for processing data
+"""
+from typing import Dict, List
+
 import numpy as np
 import torch
+from pandas import DataFrame, Series
 from scipy.stats import pearsonr
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 from torch.utils.data import Dataset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizer
 
 from src.tools.data_analysis.analysis_utils import (aggregate_by_task,
                                                     load_and_prep_dataframe)
 
 
 class ComplexityDataset(Dataset):
-    def __init__(self, data, tokenizer):
-        self.data = [
-            (cntx, cmplx, trgt) for (cntx, cmplx, trgt) in zip(data["contexts"], data["scores"], data["targets"])]
+    """
+    Base dataset class for creating pairs contexts - complexity score.
+
+    Args:
+        data: dictionary with Series with contexts, scores and target words.
+        tokenizer: pretrained model's tokenizer from Transformers repo.
+    """
+    def __init__(
+            self, data: Dict[str, Series], tokenizer: PreTrainedTokenizer
+            ) -> None:
+        self.data = list(
+            zip(data["contexts"], data["scores"], data["targets"]))
         self.tokenizer = tokenizer
         self.tokenized_data = self._tokenize_and_align_labels()
 
-    def _tokenize_and_align_labels(self):
+    def _tokenize_and_align_labels(self) -> List:
+        """
+        Tokenizes data with self.tokenizer and packs it
+        with complexity scores and attention masks.
+        """
         tokenized_data = []
         for context, score, target in self.data:
-            tokenized_inputs = self.tokenizer(text=context, text_pair=target, truncation=True, padding='max_length', return_tensors='pt', max_length=48)
-            tokenized_inputs["labels"] = np.array(score)
-            tokenized_inputs["labels"] = tokenized_inputs["labels"].astype(np.float32)
-            tokenized_inputs['attention_mask'] = torch.squeeze(tokenized_inputs['attention_mask']).tolist()
-            tokenized_inputs['input_ids'] = torch.squeeze(tokenized_inputs['input_ids']).tolist()
-            tokenized_inputs['token_type_ids'] = torch.squeeze(tokenized_inputs['token_type_ids']).tolist()
+            tokenized_inputs = self.tokenizer(
+                text=context, text_pair=target, truncation=True,
+                padding='max_length', return_tensors='pt', max_length=48)
+            tokenized_inputs["labels"] = np.array(score).astype(np.float32)
+            tokenized_inputs['attention_mask'] = torch.squeeze(
+                tokenized_inputs['attention_mask']).tolist()
+            tokenized_inputs['input_ids'] = torch.squeeze(
+                tokenized_inputs['input_ids']).tolist()
+            tokenized_inputs['token_type_ids'] = torch.squeeze(
+                tokenized_inputs['token_type_ids']).tolist()
             tokenized_data.append(tokenized_inputs)
         return tokenized_data
 
@@ -32,12 +55,25 @@ class ComplexityDataset(Dataset):
         return self.tokenized_data[idx]
 
     def get_data(self, idx):
+        """
+        Returns sample of original data (not tokenized).
+        """
         return self.data[idx]
 
     def __len__(self):
         return len(self.data)
 
-    def from_indices(self, indices):
+    def from_indices(self, indices: List[int]) -> "ComplexityDataset":
+        """
+        Creates new Dataset class with data sampled
+        by given indices.
+
+        Args:
+            indices: indices of data samples to include in new class.
+
+        Returns:
+            new instance of ComplexityDataset
+        """
         data = {
             "contexts": [],
             "scores": [],
@@ -50,11 +86,25 @@ class ComplexityDataset(Dataset):
         return ComplexityDataset(data, self.tokenizer)
 
 
-def get_tokenizer(name: str):
-  return AutoTokenizer.from_pretrained(name)
+def get_tokenizer(name: str) -> PreTrainedTokenizer:
+    """
+    Loads pretrained Tokenizer from Transformers repo.
+    """
+    return AutoTokenizer.from_pretrained(name)
 
 
-def prepare_dataset(pools_folder, initial_df):
+def prepare_dataset(
+        pools_folder: str,
+        initial_df: DataFrame
+        ) -> Dict[str, Series]:
+    """
+    Preprocesses original dataset to use in training
+    in the future.
+
+    Args:
+        pools_folder: directory with annotation results (tsv) from toloka.
+        initial_df: tsv file with all sentences and their data.
+    """
     dataframe = load_and_prep_dataframe(pools_folder, initial_df)
     dataframe = aggregate_by_task(dataframe, auxiliary_mapping={
         "target_word": "first",
@@ -62,7 +112,7 @@ def prepare_dataset(pools_folder, initial_df):
     })
     targets = dataframe["target_word"]
     contexts = dataframe["INPUT:text"].map(
-        lambda x: x.replace("<mark>","").replace("</mark>",""))
+        lambda x: x.replace("<mark>", "").replace("</mark>", ""))
     scores = dataframe["OUTPUT:complexity"]
     dataset = {
         "contexts": contexts,
@@ -73,26 +123,31 @@ def prepare_dataset(pools_folder, initial_df):
 
 
 def run_on_validation_set(model, test_dataset):
+    """
+    Runs model on test dataset and print predictions.
+    """
     for inputs in test_dataset:
-        inputs['input_ids'] = torch.unsqueeze(inputs['input_ids'], 0).to("cuda")
-        inputs['attention_mask'] = torch.unsqueeze(inputs['attention_mask'], 0).to("cuda")
-        # inputs['token_type_ids'] = torch.unsqueeze(inputs['token_type_ids'], 0).to("cuda")
-
-        # result = model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'], token_type_ids=inputs['token_type_ids'])
-        result = model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
-        print(result.logits.item(), inputs['labels'].item(), inputs['sentence'])
+        inputs['input_ids'] = torch.unsqueeze(
+            inputs['input_ids'], 0).to("cuda")
+        inputs['attention_mask'] = torch.unsqueeze(
+            inputs['attention_mask'], 0).to("cuda")
+        result = model(
+            input_ids=inputs['input_ids'],
+            attention_mask=inputs['attention_mask'])
+        sentences = inputs["sentence"]
+        print(result.logits.item(), inputs['labels'].item(), sentences)
 
 
 def compute_metrics(pred):
-    labels = [x for x in pred.label_ids]
+    """
+    Computes MAE and Pearson Correlation Coefficient
+    on given predictions and ground truth data.
+    """
+    labels = pred.label_ids.tolist()
     preds = [x[0] for x in pred.predictions]
-    # calculate accuracy using sklearn's function
-    # print(f"labels : {labels[:10]}")
-    # print(f"pred : {preds[:50]}")
     mae = mean_absolute_error(labels, preds)
     mape = mean_absolute_percentage_error(labels, preds)
-    # correlation = np.corrcoef([labels, preds])[0, 1]
-    corr, pval = pearsonr(labels, preds)
+    corr, _ = pearsonr(labels, preds)
     return {
         'mean_absolute_error': mae,
         'mean_absolute_percentage_error': mape,
--- src/model_training/model_utils.py
+++ src/model_training/model_utils.py
@@ -1,8 +1,23 @@
-from transformers import (BertForSequenceClassification,
+"""
+Module with utils for model loading
+"""
+
+from transformers import (BertForSequenceClassification, PreTrainedModel,
                           RobertaForSequenceClassification)
 
 
-def get_model(name: str):
+def get_model(name: str) -> PreTrainedModel:
+    """
+    Load pretrained model from Transformers repo.
+
+    Args:
+        name: model name as stated in Transformers repo.
+              Available options: DeepPavlov/rubert-base-cased,
+              sberbank-ai/ruRoberta-large
+
+    Returns:
+        pretrained PyTorch model
+    """
     if name == "sberbank-ai/ruRoberta-large":
         model = RobertaForSequenceClassification.from_pretrained(
             name, num_labels=1)
@@ -10,5 +25,7 @@ def get_model(name: str):
         model = BertForSequenceClassification.from_pretrained(
             name, num_labels=1)
     else:
-        raise ValueError("Incorrect model name, available are: DeepPavlov/rubert-base-cased, sberbank-ai/ruRoberta-large")
+        raise ValueError(
+            "Incorrect model name, available are:"
+            "DeepPavlov/rubert-base-cased, sberbank-ai/ruRoberta-large")
     return model
